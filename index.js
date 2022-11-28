8307ec2918625e8e37e5c640c54686677fc92fb2
samethozman <samet.hozman@outlook.com>
2022-11-28 13:31:37 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity){
+    if (activity === undefined) {activity = `roller-skate`}
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(job) {
+    if (job === undefined) {job = `go to the office`}
+    return `This Monday, I will ${job}.`
+
+}
+
+function wrapAdjective(thingToWrap){
+    if (thingToWrap === undefined){thingToWrap = "*"}
+    const innerFunction = function(nicePhrase){
+        if (nicePhrase === undefined) {nicePhrase = "special"}
+        return `You are ${thingToWrap}${nicePhrase}${thingToWrap}!`
+
+    }
+    return innerFunction
+}
